24a066bf52f894731cf79d6ae708b5851df9b524
Damir Akhmadullin <damir@damir.local>
2023-07-29 19:49:19 +0300

    aliases for absolute import

--- svelte.config.js
+++ svelte.config.js
@@ -2,9 +2,14 @@ import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter()
-  },
-  preprocess: vitePreprocess()
+	kit: {
+		adapter: adapter(),
+		alias: {
+			// an alias ending /* will only match
+			// the contents of a directory, not the directory itself
+      '@types': "./src/types/index.ts",
+		}
+	},
+	preprocess: vitePreprocess()
 };
 export default config;
