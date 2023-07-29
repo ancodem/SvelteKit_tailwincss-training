d74f7979b1edd9f39aebbf2bb76b76297ce3684d
Damir Akhmadullin <damir@damir.local>
2023-07-29 19:49:58 +0300

    component for centering future works

--- src/lib/Center.svelte
+++ src/lib/Center.svelte
@@ -1,3 +1,3 @@
-<section class="w-screen h-screen flex justify-center items-center">
-	<slot class="mx-auto"/>
+<section class="w-full h-full flex justify-center items-center">
+	<slot class="mx-auto" />
 </section>
